99267d63f37090d4ed9e32b1b816f317546a142c
Ahmad Alfikri Nadrian <82949202+nadrianfikri@users.noreply.github.com>
2021-09-22 13:04:26 +0000

    improve generator

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const displayNum = document.getElementById('display-num');
+const displayNum = document.querySelector('#display-num p');
+const displayElm = document.getElementById('display-num');
 const numberInput = document.getElementById('number-input');
 
 function performEvenNumber() {
   let result = '';
   for (let i = 1; i <= numberInput.value; i++) {
     if (i % 2 === 0) {
-      result += `<p>${i}<p>`;
+      result += `${i}  `;
     }
   }
 
-  displayNum.style.height = 'auto';
-  displayNum.style.border = '3px solid #9c1515 ';
+  // displayNum.style.height = 'auto';
+  displayElm.style.border = '3px solid #9c1515 ';
 
   displayNum.innerHTML = result;
 }
@@ -18,11 +19,11 @@ function performOddNumber() {
   let result = '';
   for (let i = 1; i <= numberInput.value; i++) {
     if (i % 2 === 1) {
-      result += `<p>${i}<p>`;
+      result += `${i}  `;
     }
   }
-  displayNum.style.height = 'auto';
-  displayNum.style.border = '3px solid #f3ef18 ';
+  // displayNum.style.height = 'auto';
+  displayElm.style.border = '3px solid #f3ef18 ';
   
   displayNum.innerHTML = result;
 }
@@ -41,13 +42,13 @@ function performPrimeNumber() {
 
   for (let i = 2; i <= numberInput.value; i++) {
     if (isPrime(i)) {
-      result += `<p>${i}<p>`;
+      result += `${i}  `;
     }
   }
   // displayNum.style.transition = '1s';
 
-  displayNum.style.height = 'auto';
-  displayNum.style.border = '3px solid #27bd27 ';
+  // displayNum.style.height = 'auto';
+  displayElm.style.border = '3px solid #27bd27 ';
 
   displayNum.innerHTML = result;
 }
